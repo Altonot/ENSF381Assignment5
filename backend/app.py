d1a16c59553bc47617f2c85b38273b6339dd6fde
Altinb <altin_15.2004@hotmail.com>
2025-04-06 19:51:55 -0600

    adding remaining backend routes

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,38 @@ import joblib
 app = Flask(__name__)
 CORS(app)
 
-students = [['Student_ID', 'Student_Username', 'Student_Password', 'Email', ['Enrolled_course1', 'Enrolled_course2']]]
+students = [
+            [
+                'Student_ID',
+                'Student_Username',
+                'Student_Password',
+                'Email', 
+                ['Enrolled_course1', 'Enrolled_course2']
+            ]
+           ]
 
-app.route('/test', methods=['POST'])
-def test():
+app.route('/register', methods=['POST'])
+def register():
+    return
+
+app.route('/login', methods=['POST'])
+def login():
+    return
+
+app.route('/testimonials', methods=['GET'])
+def returnRandomTestimonials():
+    return
+
+app.route('/enroll/<student_id>', methods=['POST'])
+def enroll():
+    return
+
+app.route('/drop/<student_id>', methods=['DELETE'])
+def drop():
+    return
+
+app.route('/courses', methods=['GET'])
+def returnCourses():
     return
 
 if __name__=='__main__':
