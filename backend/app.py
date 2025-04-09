7a2e3b8e5c99d67a9a32ebbea10fb6732f25e616
Alt <144161125+Altonot@users.noreply.github.com>
2025-04-09 13:51:00 -0600

    coding app routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import json, copy
 
 app = Flask(__name__)
 CORS(app)
@@ -15,29 +16,53 @@ students = [
             ]
            ]
 
-app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register():
     return
 
-app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     return
 
-app.route('/testimonials', methods=['GET'])
+@app.route('/testimonials', methods=['GET'])
 def returnRandomTestimonials():
     return
 
-app.route('/enroll/<student_id>', methods=['POST'])
-def enroll():
-    return
+@app.route('/enroll/<student_id>', methods=['POST'])
+def enroll(student_id):
+    courseInfo = request.json()
+    enrolled = False
+    for i in range(len(students)):
+        if students[i][0] == student_id:
+            students[i][4].append(courseInfo)
+            enrolled = True
+    return enrolled
 
-app.route('/drop/<student_id>', methods=['DELETE'])
-def drop():
-    return
+@app.route('/drop/<student_id>', methods=['DELETE'])
+def drop(student_id):
+    courseInfo = request.json()
+    dropped = False
+    for i in range(len(students)):
+        if students[i][0] == student_id:
+            for j in range(len(students[i][4])):
+                if courseInfo == students[i][4][j]:
+                    students[i][4].pop(j)
+                    dropped = True
+    return dropped
 
-app.route('/courses', methods=['GET'])
+@app.route('/courses', methods=['GET'])
 def returnCourses():
-    return
+    with open("courses.json", "r") as file:
+        data = json.load(file)
+        courses = data['courses']
+    return courses
+
+@app.route('/student_courses/<student_id>', methods=['GET'])
+def returnStudentCourses(student_id):
+    for i in range(len(students)):
+        if students[i][0] == student_id:
+            courses = copy.deepcopy(students[i][4])
+    return courses
 
 if __name__=='__main__':
     app.run()
